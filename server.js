2a232650895233e3f5c19bfe1632232543ab063a
Philippe <philippe.idalgoprestes@gmail.com>
2023-06-23 15:06:21 -0300

    Implementação de GET e POST final

--- server.js
+++ server.js
@@ -3,33 +3,40 @@ import cors from 'cors'
 
 const server = express();
 server.use(cors());
+server.use(express.json());
 server.listen(5000, () => console.log('Running on port 5000'));
 
-var users = {};
+var users = [];
 var tweets = [];
 
 server.post("/tweets", (req, res) => {
     // Verifica se o usuário da request está cadastrado e pega o avatar
-    const r = req.params
-    for (let a = 0; a < users.length; a++){
-        if (users[a].username == r.username){
-            // Adiciona o tweet ao objeto
-            const obj = { username: r.username, avatar: users[a].avatar, tweet: r.tweet}
-            Object.assign(tweets, obj);
-            res.status(200);
-            res.send('OK');
-            break;
+    const r = req.body;
+    if (users.length != 0){
+        for (let a = 0; a < users.length; a++){
+            if (users[a].username == r.username){
+                // Adiciona o tweet ao objeto
+                const obj = { username: r.username, avatar: users[a].avatar, tweet: r.tweet}
+                tweets.push(obj);
+                res.status(200);
+                res.send('OK');
+                break;
+            }
+            else if ((a == users.length - 1 || a == users.length) && users[a].username != r.username){
+                res.status(202);
+                res.send('UNAUTHORIZED');
+            }
         }
-        else if (a == users.length - 1 && users[a].username != r.username){
-            res.status(202);
-            res.send('UNAUTHORIZED');
-        }
+    }
+    else{
+        res.status(202);
+        res.send('UNAUTHORIZED');
     }
 })
 
 server.post("/sign-up", (req, res) => {
     // Salva em users
-    Object.assign(users, req);
+    users.push(req);
     res.status(200);
     res.send('OK');
 })
